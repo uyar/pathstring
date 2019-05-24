9222c1aa1d8e536d982075f2cf8ed43bdbb530ef
H. Turgut Uyar <uyar@tekir.org>
2019-05-24 14:50:22 +0300

    test: Add root and parent tests

--- tests/test_path.py
+++ tests/test_path.py
@@ -83,6 +83,58 @@ def test_unc_shares_should_be_considered_as_drives_on_windows():
     assert Path("\\\\host\\share\\foo.txt").drive == "\\\\host\\share"
 
 
+@mark.skipif(sys.platform == "win32", reason="posix behaviour")
+def test_root_should_be_separator_on_posix():
+    assert Path("/etc").root == sep
+
+
+@mark.skipif(sys.platform != "win32", reason="windows behaviour")
+def test_root_should_be_separator_on_windows():
+    assert Path("c:\\Program Files\\").root == sep
+
+
+@mark.skipif(sys.platform != "win32", reason="windows behaviour")
+def test_drive_should_not_be_part_of_root_on_windows():
+    assert Path("c:Program Files\\").root == ""
+
+
+@mark.skipif(sys.platform != "win32", reason="windows behaviour")
+def test_unc_shares_should_always_have_a_root_on_windows():
+    assert Path("\\\\host\\share\\").root == "\\"
+
+
+def test_parents_should_be_a_sequence_of_paths():
+    assert list(Path("/foo/bar/setup.py").parents) == [
+        "/foo/bar".replace("/", sep),
+        "/foo".replace("/", sep),
+        "/".replace("/", sep),
+    ]
+
+
+@mark.skipif(sys.platform != "win32", reason="windows behaviour")
+def test_parents_should_include_drive_on_windows():
+    assert list(Path("c:\\foo\\bar\\setup.py").parents) == ["c:\\foo/bar", "c:\\foo", "c:\\"]
+
+
+def test_parent_should_be_parent_directory():
+    assert Path("/a/b/c/d").parent == "/a/b/c".replace("/", sep)
+
+
+def test_parent_should_not_go_past_anchor():
+    assert Path("/").parent == sep
+
+
+def test_parent_should_not_go_past_empty_path():
+    assert Path(".").parent == "."
+
+
+def test_parent_should_be_lexical_operation():
+    assert Path("foo/..").parent == "foo"
+
+
+# TODO: ".anchor" tests
+
+
 def test_relative_to_target_starting_with_parent_folder_should_fail_when_strict():
     with raises(ValueError):
         Path("d1", "d2", "f").relative_to(Path("d1", "d3"))
